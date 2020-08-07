11872dfd2eeb375df8dafc7ce684a6cddadd29ee
Wilson <wilsonhorrell@git.training>
2020-08-07 13:22:33 -0500

    Initial commit

---
+++ db/connection.js
@@ -0,0 +1,16 @@
+const util = require('util');
+const mysql = require('mysql')
+connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'kimsey123',
+    database: 'employees'
+});
+
+connection.connect();
+
+// Setting up connection.query to use promises instead of callbacks
+// This allows us to use the async/await syntax
+connection.query = util.promisify(connection.query);
+
+module.exports = connection;
